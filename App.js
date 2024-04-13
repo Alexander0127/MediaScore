a7590f42ad738534c227440815f4248442e5e0c7
Alexander0127 <AlexanderTR0127@gmail.com>
2024-04-12 23:57:38 -0500

    Cambio de prueba

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import React from 'react'
 export default function App() {
   return (
     <View className="flex items-center justify-center w-full h-screen bg-slate-950">
+      <Text className="text-slate-50">App123</Text>
       <Text className="text-slate-50">App</Text>
       
     </View>
